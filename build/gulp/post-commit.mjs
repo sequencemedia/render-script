c9e3ce950eefb1b335e799ef075ade09e130f9ae
Jonathan Perry <jonathanperry@sequencemedia.net>
2021-12-29 00:02:09 +0000

    Post-commit

--- build/gulp/post-commit.mjs
+++ build/gulp/post-commit.mjs
@@ -12,10 +12,52 @@ const OPTIONS = {
   maxBuffer: 1024 * 500
 }
 
+const BRANCH = 'master'
+
 const log = debug('render-script:build:gulp:post-commit')
 const error = debug('render-script:build:gulp:post-commit:error')
 
 log('`render-script` is awake')
+
+const PACKAGE_VERSION_CHANGES = /\-+\s+"version":\s+"(\d+\.\d+\.\d+)",+\s+\++\s+"version":\s+"(\d+\.\d+\.\d+)",+\s+/ /* eslint-disable-line no-useless-escape */
+
+const trim = (v) => v.split('\n').map((v) => v.trimEnd()).join('\n').trim()
+
+function use (key) {
+  const log = debug(`render-script:${key}`)
+
+  return function use (v) {
+    log(trim(v))
+  }
+}
+
+export function getGitRemoteShowOriginHeadBranch () {
+  log('getGitRemoteShowOriginHeadBranch')
+
+  return (
+    new Promise((resolve, reject) => {
+      const command = 'git remote show origin | awk \'/HEAD branch/ {print $NF}\''
+
+      const {
+        stdout,
+        stderr
+      } = exec(command, OPTIONS, (e, v = '') => (!e) ? resolve(trim(v)) : reject(e))
+
+      stdout.on('data', use('git-remote-show-origin-head-branch'))
+      stderr.on('data', use('git-remote-show-origin-head-branch'))
+    })
+  )
+}
+
+export function notPackageVersionChanges (branch = BRANCH) {
+  log('notPackageVersionChanges')
+
+  return (
+    new Promise((resolve, reject) => {
+      exec(`git diff HEAD origin/${branch} package.json`, OPTIONS, (e, v) => (!e) ? resolve(PACKAGE_VERSION_CHANGES.test(v) !== true) : reject(e))
+    })
+  )
+}
 
 export async function getPackageVersion () {
   log('getPackageVersion')
@@ -41,7 +83,9 @@ export default async function postCommit () {
   log('post-commit')
 
   try {
-    await gitTag(await getPackageVersion())
+    if (await notPackageVersionChanges(await getGitRemoteShowOriginHeadBranch())) {
+      await gitTag(await getPackageVersion())
+    }
   } catch ({ code = 'NONE', message = 'No error message defined' }) {
     error({ code, message })
   }
