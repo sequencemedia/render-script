f8ae60be3c8ef120666c53d8dfe48e5513d5d6f5
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-07-08 14:57:40 +0100

    `mjs` `cjs`

--- src/index.cjs
+++ src/index.cjs
@@ -1,11 +1,35 @@
-require('@babel/register')({
-  ignore: [
-    /node_modules\/(?!render-script)/
-  ]
-})
-
 const {
-  default: evaluate
-} = require('./index.mjs')
+  isTruthy,
+  getRuleMap,
+  getConfigurationMap,
+  evaluate: evaluateRuleForConfiguration
+} = require('./evaluate.cjs')
 
-module.exports = evaluate
+function getEvaluation (configurationMap = new Map(), rule = '', configuration = '') {
+  /**
+   *  log('getEvaluation')
+   */
+
+  let evaluation
+
+  /**
+   *  Does the `configurationMap` contain the `configuration` argument as a key?
+   */
+  if (configurationMap.has(configuration)) {
+    /**
+     *  It does! Get the evaluation from the `configurationMap`
+     */
+    evaluation = configurationMap.get(configuration)
+  } else {
+    /**
+     *  It doesn't. Get the evaluation from `evaluateRuleForConfiguration` and set it into the `configurationMap`
+     */
+    configurationMap.set(configuration, (evaluation = evaluateRuleForConfiguration(rule, configuration)))
+  }
+
+  return evaluation
+}
+
+module.exports = function evaluate (rule = '', configuration = '') {
+  return isTruthy(rule) ? isTruthy(configuration) ? getEvaluation(getConfigurationMap(getRuleMap(), rule), rule, configuration) : false : false
+}
